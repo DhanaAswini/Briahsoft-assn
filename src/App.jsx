35b27a314e9261563cfe2b3757b91789b4c6d1e0
DhanaAswini <dhanaaswinii@gmail.com>
2025-04-06 21:27:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import WSIViewer from "./components/WSIViewer";
+
+function App() {
+  return (
+    <div className="h-screen">
+      <WSIViewer />
+    </div>
+  );
+}
+
+export default App;
